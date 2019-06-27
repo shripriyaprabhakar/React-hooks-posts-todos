0a722991df48b3ad971a7f93a3cc93a211f1e469
priya suresh <priyasuresh@MacBook-Pro-5.local>
2019-06-27 14:33:00 -0700

    changed life cycle methods to hooks

--- simple-hooks/src/components/ResourceList.js
+++ simple-hooks/src/components/ResourceList.js
@@ -1,33 +1,19 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends Component {
-  state = {
-    resources: []
-  };
-  async componentDidMount() {
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResource = async resource => {
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
-    this.setState({ resources: response.data });
-  }
-
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      this.setState({ resources: response.data });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div>{this.state.resources.length}</div>
-      </div>
-    );
-  }
-}
+    setResources(response.data);
+  };
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
+  return <div>{resources.length}</div>;
+};
 
 export default ResourceList;
