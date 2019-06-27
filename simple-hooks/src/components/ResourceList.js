9842efcdfb1a7be818ca08d7ac8ccab8d7f4d48b
priya suresh <priyasuresh@MacBook-Pro-5.local>
2019-06-27 13:54:27 -0700

    app converted to hook

---
+++ simple-hooks/src/components/ResourceList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ResourceList extends Component {
+  render() {
+    return (
+      <div>
+        <div>hello</div>
+      </div>
+    );
+  }
+}
+
+export default ResourceList;
